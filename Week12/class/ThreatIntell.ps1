a2843afd564a41c61b6fc91c1407e53455f2973d
T20A026 <abijah.buttendorf@mymail.champlain.edu>
2022-11-17 01:19:20 -0500

    Week 12 Class Commit

---
+++ Week12/class/ThreatIntell.ps1
@@ -0,0 +1,33 @@
+# Extract Ip's
+
+$drop_urls = @('https://rules.emergingthreats.net/blockrules/emerging-botcc.rules','https://rules.emergingthreats.net/blockrules/compromised-ips.txt')
+
+foreach ($u in $drop_urls) {
+
+    $temp = $u.split("/")
+
+    $file_name = $temp[-1]
+
+    if (Test-Path $file_name) {
+        continue
+    } else {
+
+    Invoke-WebRequest -uri $u -Outfile $file_name
+    
+        }
+}
+
+#Filenames Array
+$input_paths = @('.\emerging-botcc.rules','emerging-botcc.rules')
+
+# Regext Ip Extractor
+$regex_drop = '\b\d{1,3}\.d{1,3}\.d{1,3}\.d{1,3}\b'
+
+# Appent IP's to temp list, and append files
+select-string -Path $input_paths -Pattern $regex_drop |
+ForEach-Object {$_.Matches}
+ForEach-Object {$_.value} | Sort-Object
+Out-File -FilePath "ips-bad.tmp"
+
+(Get-Content -Path ".\ips-bad.tmp") | ForEach-Object 
+{ $_ -replace "^","iptables -A INPUT -s" -replace "$"," -j DROP"}
---
+++ Week12/class/console.ps1
@@ -0,0 +1,23 @@
+# Login to a remote SSH server
+#New-SSHSession -ComputerName '10.0.0.4' -Credential (Get-Credential root)
+<#
+
+Im a Multiline Comment!!!!
+
+#>
+<#
+while ($true) {
+
+    # Add a prompt to run commands
+    $cmdVar = read-host -Promt "Please enter a command"
+
+    # Run command on remote SSH Server
+    (Invoke-SSHCommand -Index 0 $cmdVar).Output
+
+}
+#>
+
+Set-SCPItem -Computername '10.0.0.4' -Credential (Get-Credential root) `
+-Destination '/home' -Path 'C:\Users\abija\Documents\VisualStudio\SYS-320-Automation\Week12\class\meme.jpg'
+
+#Remove-SSHSession -SessionId 0
